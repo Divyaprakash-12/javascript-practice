6ca6ef0104f2b326babe1ad1f7aeefd2bf60e5c3
Divyaprakash-12 <prakashhari140@gmail.com>
2023-05-11 17:02:53 +0530

    commit

---
+++ myfunc.js
@@ -0,0 +1,9 @@
+function myfunc(){
+    document.getElementById("para1").innerHTML= "paragraph changed";
+    document.getElementById("h1").innerHTML="welcome dp";
+    console.log("java script working"); 
+}
+
+function hello1(){
+    document.write("hello dp")
+}
